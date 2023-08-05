24cc16f16e6e103f2519084caf60b4b3369d6629
Matias Moglia <matiasmoglia@gmail.com>
2023-08-05 15:55:38 -0300

    adding workflow

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -3,3 +3,19 @@ resource "aws_ssm_parameter" "foo" {
   type  = "String"
   value = "bar"
 }
+
+data "archive_file" "python_lambda_package" {
+  type        = "zip"
+  source_file = "${path.module}/../lambda/main.py"
+  output_path = "lambda_main.zip"
+}
+
+resource "aws_lambda_function" "hello_lambda_function" {
+  function_name    = "HelloWorld"
+  filename         = "lambda_main.zip"
+  source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
+  role             = aws_iam_role.lambda_role.arn
+  runtime          = "python3.9"
+  handler          = "main.lambda_handler"
+  timeout          = 10
+}
