dfb5b18d282f5d7b3cf1d6bcd43d86d3097704ab
Matias Moglia <matiasmoglia@gmail.com>
2023-08-06 16:45:24 -0300

    adding new lambda

--- src/lambda/main.py
+++ src/lambda/main.py
@@ -18,6 +18,71 @@ def get_instance_private_ip(instance_id, instance_state):
     return hostname, private_ip
 
 
+def create_or_update_dns_record(hostname, private_ip):
+
+    route53 = boto3.client('route53')
+
+    response = route53.change_resource_record_sets(
+            HostedZoneId='Z03897701QHUM8QRP9QGT',
+            ChangeBatch={
+                "Comment": "Updated by Lambda DDNS",
+                "Changes": [
+                    {
+                        "Action": "UPSERT",
+                        "ResourceRecordSet": {
+                            "Name": hostname + '.customzone.local',
+                            "Type": 'A',
+                            "TTL": 60,
+                            "ResourceRecords": [
+                                {
+                                    "Value": private_ip
+                                },
+                            ]
+                        }
+                    },
+                ]
+            }
+        )
+    
+    return response
+
+
+def delete_dns_record(hostname, private_ip):
+
+    route53 = boto3.client('route53')
+
+    response = route53.list_resource_record_sets(
+        HostedZoneId='Z03897701QHUM8QRP9QGT',
+        StartRecordName=hostname + '.customzone.local'
+    )
+
+    dns_private_ip = response['ResourceRecordSets'][0]['ResourceRecords'][0]['Value']
+
+    response = route53.change_resource_record_sets(
+            HostedZoneId='Z03897701QHUM8QRP9QGT',
+            ChangeBatch={
+                "Comment": "Updated by Lambda DDNS",
+                "Changes": [
+                    {
+                        "Action": "DELETE",
+                        "ResourceRecordSet": {
+                            "Name": hostname + '.customzone.local',
+                            "Type": 'A',
+                            "TTL": 60,
+                            "ResourceRecords": [
+                                {
+                                    "Value": dns_private_ip
+                                },
+                            ]
+                        }
+                    },
+                ]
+            }
+        )
+    
+    return response
+
+
 def lambda_handler(event, context):
 
     instance_id, instance_state = event['detail']['instance-id'], event['detail']['state']
@@ -26,8 +91,12 @@ def lambda_handler(event, context):
 
     if instance_state == 'terminated':
         print("launch cleanup", hostname, private_ip)
+        output = delete_dns_record(hostname, private_ip)
+        print(output)
     else:
         print("launch create/update", hostname, private_ip)
+        output = create_or_update_dns_record(hostname, private_ip)
+        print(output)
 
     return {
         'statusCode' : 200,
