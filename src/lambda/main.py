7d408ab47e0388245400a0c716a7df7cb51781c0
Matias Moglia <matiasmoglia@gmail.com>
2023-08-06 13:35:11 -0300

    change deploy to destroy in destroy workflow

--- src/lambda/main.py
+++ src/lambda/main.py
@@ -1,9 +1,21 @@
 import boto3
+import json
 
 
 def lambda_handler(event, context):
-    print("EVENT", event)
-    print("CONTEXT", context)
+
+    instance_id = json.loads(event['detail']['instance-id'])
+    instance_state = json.loads(event['detail']['state'])
+    print(f"instance ID: {instance_id}")
+
+    ec2 = boto3.resource('ec2')
+
+    response = ec2.describe_instances(
+        InstanceIds=[instance_id],
+    )
+
+    print("RESPONSE", response)
+
     result = "Hello World"
     return {
         'statusCode' : 200,
