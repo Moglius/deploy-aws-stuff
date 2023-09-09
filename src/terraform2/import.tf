bd2ce0665adf30cf6b122335e690a7c1c018801c
Matias <matiasmoglia@gmail.com>
2023-09-09 15:33:27 -0300

    Update import.tf

--- src/terraform2/import.tf
+++ src/terraform2/import.tf
@@ -1,15 +1,1 @@
-import {
-  to = aws_instance.server["mgn1"]
-  id = "i-00f1a0c2e4e9ce93c"
-}
-
-import {
-  to = aws_instance.server["mgn2"]
-  id = "i-012fcda28ce798697"
-}
-
-import {
-  to = aws_instance.server["mgn3"]
-  id = "i-08f21ed02e96df32f"
-}
 
